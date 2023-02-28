e0457023b59e436d9c90a08f2d26ff2f83fd2c24
Ellmist <ellimistdev@gmail.com>
2023-02-28 12:40:27 -0800

    Test new UI syntax for message templates.

--- MessageTemplates/MessageTemplates.lua
+++ MessageTemplates/MessageTemplates.lua
@@ -1,4 +1,13 @@
 MessageTemplateMixin = {}
+MESSAGEBARKER_BACKDROP_TOOLTIP_16_16_5555 = {
+    bgFile = "Interface\\Tooltips\\UI-Tooltip-Background",
+    edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
+    tile = true,
+    tileEdge = true,
+    tileSize = 16,
+    edgeSize = 16,
+    insets = { left = 5, right = 5, top = 5, bottom = 5 },
+};
 
 function MessageTemplateMixin:UpdateLayout() end
 
@@ -6,4 +15,3 @@ function MessageTemplateMixin:SetMessage(message)
 	assert(message)
 	self.message = message
 end
-
